23b45bb26ef60991078fc179112a1f5c7e09f0c7
Nguyen Ngo Cao Cuong <cuong35228102000@gmail.com>
2021-06-02 08:23:08 +0700

    Semantic 4

---
+++ Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/incompleted/charcode.c" "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/incompleted/charcode.c.obj"
+  "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/incompleted/debug.c" "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/incompleted/debug.c.obj"
+  "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/incompleted/error.c" "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/incompleted/error.c.obj"
+  "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/incompleted/main.c" "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/incompleted/main.c.obj"
+  "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/incompleted/parser.c" "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/incompleted/parser.c.obj"
+  "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/incompleted/reader.c" "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/incompleted/reader.c.obj"
+  "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/incompleted/scanner.c" "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/incompleted/scanner.c.obj"
+  "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/incompleted/semantics.c" "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/incompleted/semantics.c.obj"
+  "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/incompleted/symtab.c" "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/incompleted/symtab.c.obj"
+  "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/incompleted/token.c" "C:/Users/cuong/CLionProjects/Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/incompleted/token.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Semantics_Analyzer 4/cmake-build-debug/CMakeFiles/untitled5.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled5.dir/incompleted/charcode.c.obj"
+  "CMakeFiles/untitled5.dir/incompleted/debug.c.obj"
+  "CMakeFiles/untitled5.dir/incompleted/error.c.obj"
+  "CMakeFiles/untitled5.dir/incompleted/main.c.obj"
+  "CMakeFiles/untitled5.dir/incompleted/parser.c.obj"
+  "CMakeFiles/untitled5.dir/incompleted/reader.c.obj"
+  "CMakeFiles/untitled5.dir/incompleted/scanner.c.obj"
+  "CMakeFiles/untitled5.dir/incompleted/semantics.c.obj"
+  "CMakeFiles/untitled5.dir/incompleted/symtab.c.obj"
+  "CMakeFiles/untitled5.dir/incompleted/token.c.obj"
+  "libuntitled5.dll.a"
+  "untitled5.exe"
+  "untitled5.exe.manifest"
+  "untitled5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
