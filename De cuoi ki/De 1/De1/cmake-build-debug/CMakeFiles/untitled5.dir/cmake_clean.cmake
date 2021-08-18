3d62048baa24e6acd53dec383674930b9fd57ca2
nguyenngocaocuong <nguyenngocaocuong@gmail.com>
2021-08-18 17:36:27 +0700

    De cuoi ki

---
+++ De cuoi ki/De 1/De1/cmake-build-debug/CMakeFiles/untitled5.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled5.dir/incompleted/charcode.c.o"
+  "CMakeFiles/untitled5.dir/incompleted/debug.c.o"
+  "CMakeFiles/untitled5.dir/incompleted/error.c.o"
+  "CMakeFiles/untitled5.dir/incompleted/main.c.o"
+  "CMakeFiles/untitled5.dir/incompleted/parser.c.o"
+  "CMakeFiles/untitled5.dir/incompleted/reader.c.o"
+  "CMakeFiles/untitled5.dir/incompleted/scanner.c.o"
+  "CMakeFiles/untitled5.dir/incompleted/semantics.c.o"
+  "CMakeFiles/untitled5.dir/incompleted/symtab.c.o"
+  "CMakeFiles/untitled5.dir/incompleted/token.c.o"
+  "untitled5"
+  "untitled5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
