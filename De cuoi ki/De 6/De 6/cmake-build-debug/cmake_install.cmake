88f890a46ade4bc524b0a90aed14a9dd73a32b87
nguyenngocaocuong <nguyenngocaocuong@gmail.com>
2021-08-18 21:41:34 +0700

    Them De 7

--- De cuoi ki/De 6/De 6/cmake-build-debug/cmake_install.cmake
+++ De cuoi ki/De 6/De 6/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ubuntu/Màn hình nền/Study/OnTapCuoiKy/Chương trình dịch/Lý thuyết chương trình dịch/bài tập/De 9/Semantics_Analyzer 4
+# Install script for directory: /home/ubuntu/Màn hình nền/Study/OnTapCuoiKy/Chương trình dịch/Lý thuyết chương trình dịch/bài tập/ctdKPL/De cuoi ki/De 7/De 6
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ubuntu/Màn hình nền/Study/OnTapCuoiKy/Chương trình dịch/Lý thuyết chương trình dịch/bài tập/De 9/Semantics_Analyzer 4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ubuntu/Màn hình nền/Study/OnTapCuoiKy/Chương trình dịch/Lý thuyết chương trình dịch/bài tập/ctdKPL/De cuoi ki/De 7/De 6/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
